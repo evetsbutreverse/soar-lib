32ae8c05429c5e19639e3b466a29bb9719b53a31
evetsbutreverse <161958403+evetsbutreverse@users.noreply.github.com>
2025-01-27 16:11:18 +0000

    123

--- api/index.js
+++ api/index.js
@@ -7,10 +7,10 @@ app.get('/', (req, res) => {
     res.send('soar lib');
    });
  
-   app.get('soar/objectweb/asm/5.0.3/asm-5.0.3.jar', (req, res) => {
+   app.get('/org/objectweb/asm/5.0.3/asm-5.0.3.jar', (req, res) => {
     res.sendFile(path.join(__dirname, './', 'asm-all-5.0.3.jar')); 
    });
-   app.get('me/eldodebug/soar/7.1.2/soar-7.1.2.jar', (req, res) => {
+   app.get('/me/eldodebug/soar/7.1.2/soar-7.1.2.jar', (req, res) => {
      res.redirect('https://github.com/Soar-Client/Legacy-SoarClient/releases/download/v7.1.2/SoarClient.jar')
    });
 
